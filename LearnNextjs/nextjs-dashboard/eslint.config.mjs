c2401ea7e05c838a0e21b7ad69b0fdedf9ba3045
sasharity <iruogheneomaraye@gmail.com>
2026-03-18 05:48:30 +0100

    fix eslint error

--- LearnNextjs/nextjs-dashboard/eslint.config.mjs
+++ LearnNextjs/nextjs-dashboard/eslint.config.mjs
@@ -1,9 +1,28 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
+// import { defineConfig, globalIgnores } from 'eslint/config';
+// import nextVitals from 'eslint-config-next/core-web-vitals';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
-]);
+// const eslintConfig = defineConfig([
+//   ...nextVitals,
+//   globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+// ]);
 
-export default eslintConfig;
+// export default eslintConfig;
+
+
+// eslint.config.mjs
+
+import js from '@eslint/js'
+import nextPlugin from '@next/eslint-plugin-next'
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+]
