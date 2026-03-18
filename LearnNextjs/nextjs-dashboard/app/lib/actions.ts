5eb505a6a340d1bd97ad6d7b0ab9cdfa52cb3070
sasharity <iruogheneomaraye@gmail.com>
2026-03-18 05:23:52 +0100

    Edited the edit-form file

--- LearnNextjs/nextjs-dashboard/app/lib/actions.ts
+++ LearnNextjs/nextjs-dashboard/app/lib/actions.ts
@@ -85,9 +85,11 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export async function updateInvoice(
   id: string,
   prevState: State,
+
   formData: FormData,
 ) {
-  const validatedFields = UpdateInvoice.safeParse({
+    
+    const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
